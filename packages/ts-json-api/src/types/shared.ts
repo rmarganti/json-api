0eac08eb3fa943073cc1493adf2504f9bc2eb740
Marco Spengler <marco.spengler@skriptfabrik.com>
2021-01-08 12:07:37 +0100

    Provide interface for pagination links as described in JSON:API

--- packages/ts-json-api/src/types/shared.ts
+++ packages/ts-json-api/src/types/shared.ts
@@ -5,6 +5,13 @@ export interface Links {
     [index: string]: string | LinkObject;
 }
 
+export interface PaginationLinks {
+    first?: string | LinkObject | null;
+    last?: string | LinkObject | null;
+    prev?: string | LinkObject | null;
+    next?: string | LinkObject | null;
+}
+
 /**
  * A Link.
  */
