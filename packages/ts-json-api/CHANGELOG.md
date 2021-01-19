edeb5b87fbb0920c709da56f6adc2805568463c6
Marco Spengler <marco.spengler@skriptfabrik.com>
2021-01-19 15:25:13 +0100

    Ensure JSON:API compliance (#4)
    * Change meta key in LinkObject to be optional as specified in JSON:API
    * Provide interface for pagination links as described in JSON:API
    * Add usage for pagination links in top level resources

--- packages/ts-json-api/CHANGELOG.md
+++ packages/ts-json-api/CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.1] - 2021-01-08
+### Added
+- `publication links` interface as part of the JSON:API 1.0 specification
+
+### Changed
+- `meta` key in `link` object to be optional to be JSON:API 1.0 compliant
+
 ## [1.0.0] - 2020-02-17
 ### Changed
 - Moved to [TSDX](https://https://github.com/palmerhq/tsdx) for development.
--- packages/ts-json-api/src/types/shared.ts
+++ packages/ts-json-api/src/types/shared.ts
@@ -1,16 +1,36 @@
 /**
+ * A single Link.
+ */
+export type Link = string | LinkObject;
+
+/**
  * An index of Links.
  */
 export interface Links {
-    [index: string]: string | LinkObject;
+    [index: string]: Link;
 }
+
+/**
+ * Top level pagination Links.
+ */
+export interface PaginationLinks {
+    first?: Link | null;
+    last?: Link | null;
+    prev?: Link | null;
+    next?: Link | null;
+}
+
+/**
+ * The top level Links.
+ */
+export type TopLevelLinks = Links & PaginationLinks;
 
 /**
  * A Link.
  */
 export interface LinkObject {
     href: string;
-    meta: Meta;
+    meta?: Meta;
 }
 
 /**
