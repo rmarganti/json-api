f63990c5c693cc53e1cda508df9bceba51089951
Ryan Marganti <rmarganti@gannett.com>
2022-10-11 13:10:09 -0400

    repo: Move prettier config to .prettierrc.json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,12 @@ module.exports = {
     extends: [
         'react-app',
         'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
+        'plugin:prettier/recommended'
     ],
     rules: {
-        'prettier/prettier': [
-            'error',
-            { singleQuote: true, tabWidth: 4, trailingComma: 'es5' },
-        ],
+        'prettier/prettier': ['error'],
         // Temporarily disabled due to version inconsistencies
         // between tsdx & eslint-config-react-app
-        '@typescript-eslint/consistent-type-assertions': 'off',
-    },
+        '@typescript-eslint/consistent-type-assertions': 'off'
+    }
 };
